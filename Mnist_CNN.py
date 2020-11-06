9fb52ecb2f08f7c959ce150e814f62d6e4bc42ee
Joona Viitanen <joona.viitanen@tuni.fi>
2020-11-06 16:12:57 +0200

    monimutkainen malli implementoitu ja muutama lisäys harjoitusjoukon muodostamiseen

--- Mnist_CNN.py
+++ Mnist_CNN.py
@@ -1,24 +1,125 @@
+#Importit
 import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
 
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras.layers import GaussianDropout
+from tensorflow.keras.layers import BatchNormalization
+from tensorflow.keras.callbacks import EarlyStopping
+from sklearn.model_selection import train_test_split
+
+
+#%%
 # Itse käytän tätä koska Linux.
 root = '/home/tuomas/Python/DATA.ML.200/DATA.ML.200_Assignment1/'
 training_data = np.load(root + 'training_data.npy')
 
 #%%
 # Muokkaa & käytä tätä jos Windows masiina.
-training_data = np.load('training_data.npy')
+win_root = r'C:/Users/joona/OneDrive - TUNI.fi/PRML/Assignments/DATA.ML.200_Assignment1/'
+training_data = np.load(win_root + 'training_data.npy')
 
 #%%
 # Muodosta harjoitusjoukko ja sille label-joukko
-trainX = training_data / 255
-trainY = np.ones(trainX.shape[0])
-for n in range(1,10):
+trainX = training_data.astype("float32") / 255.0 #normalisointi
+# Lisää yksi ulottuvuus, jotta neuroverkko osaa vastaanottaa datan
+# (60000, 28, 28) -> (60000, 28, 28, 1)
+trainX = np.expand_dims(trainX, axis=3)
+trainY = np.ones(trainX.shape[0]).astype("int32")
+for n in range(1,11):
     trainY[(n-1)*6000 : n*6000] *= n
-    
 
+# Muokkaa labelit "one-hot encoding" muotoon neuroverkkoa varten
+trainY_input = np.zeros((len(trainY), 10)).astype("int32")
+for i in range(0, len(trainY)):
+    label = int(trainY[i]-1) # Vähennä 1, koska datan numerot 1-10 labeloidaan välille 0-9.
+    trainY_input[i][label] = 1
+
+# Harjoitus-validointi split
+trainX, validX, trainY_input, validY_input = train_test_split(trainX, 
+                                                              trainY_input, 
+                                                              test_size=0.2,
+                                                              random_state=1)
 
 #%%
 import matplotlib.pyplot as plt
 img = training_data[6000]
 plt.imshow(img, cmap='gray')
+plt.show()
+
+#%% Complex model 
+# Överi. Hieman muokattu versio introkurssin cifar10-verkosta. 
+# Saavuttaa ~99% validation accuracyn. Ei cross-validaatiota, joten mahd. overfittaa.
+dropout = 0.2
+starting_filters = 8 
+kernel = (3, 3)
+model = Sequential()
+
+model.add(Conv2D(starting_filters, kernel_size=kernel, activation='relu', 
+                 padding="same", kernel_initializer='he_uniform'))
+model.add(BatchNormalization())
+model.add(Conv2D(starting_filters, kernel_size=kernel, activation='relu', padding="same", kernel_initializer='he_uniform'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(BatchNormalization())
+model.add(GaussianDropout(dropout * 0.55))
+
+model.add(Conv2D(2*starting_filters, kernel_size=kernel, activation='relu', padding="same", kernel_initializer='he_uniform'))
+model.add(BatchNormalization())
+model.add(Conv2D(2*starting_filters, kernel_size=kernel, activation='relu', padding="same", kernel_initializer='he_uniform'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(BatchNormalization())
+model.add(GaussianDropout(dropout * 0.7))
+
+model.add(Conv2D(4*starting_filters, kernel_size=kernel, activation='relu', padding="same", kernel_initializer='he_uniform'))
+model.add(BatchNormalization())
+model.add(Conv2D(4*starting_filters, kernel_size=kernel, activation='relu', padding="same", kernel_initializer='he_uniform'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(BatchNormalization())
+model.add(GaussianDropout(dropout * 0.85))
+
+model.add(Flatten())
+model.add(Dense(4*starting_filters, activation='relu', kernel_initializer='he_uniform'))
+model.add(BatchNormalization())
+model.add(GaussianDropout(dropout))
+model.add(Dense(10, activation='softmax'))
+
+
+#%% Fitting
+epochs = 50
+batch_size = 128
+learning_rate = 0.01
+print(f"Running {epochs} epochs with batch size of {batch_size} \
+      and learning rate of {learning_rate}.")
+opt = tf.optimizers.Adam(lr=learning_rate)
+# Keskeyttää fitin, jos validation accuracy ei parane 10 epochiin.
+callback = EarlyStopping(monitor='val_accuracy', patience=10, 
+                         restore_best_weights=True, mode="max")
+model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
+history = model.fit(trainX, trainY_input, epochs=epochs, 
+                    validation_data=(validX, validY_input), verbose=2,
+                    batch_size=batch_size, use_multiprocessing=True, 
+                    workers=16, callbacks=[callback])
+'''
+#Prediction
+y_pred = np.argmax(model.predict(X_test), axis=1)
+accuracy = class_acc(y_pred, Y_test)
+print(f"Accuracy: {accuracy}")'''
+
+
+#Plotting
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.grid()
+plt.legend(['train', 'test'], loc='upper left')
 plt.show()
--- old_nn.py
+++ old_nn.py
@@ -1,9 +1,8 @@
 import numpy as np
-import time
-import pickle
 import tensorflow as tf
 import matplotlib.pyplot as plt
 import os
+from PIL import Image
 
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
@@ -13,27 +12,18 @@ from tensorflow.keras.layers import MaxPooling2D
 from tensorflow.keras.layers import GaussianDropout
 from tensorflow.keras.layers import BatchNormalization
 from tensorflow.keras.callbacks import EarlyStopping
-from PIL import Image
+from sklearn.model_selection import train_test_split
 
-
-def class_acc(pred, gt):
-    return np.average(pred == gt)
-
-
-def unpickle(file):
-    with open(file, 'rb') as f:
-        dict = pickle.load(f, encoding="latin1")
-    return dict
 
 
 def transform_labels(labels):
     new_labels = np.zeros((len(labels), 10))
     for i in range(0, len(labels)):
-        label = labels[i]
+        label = int(labels[i]-1)
         new_labels[i][label] = 1
     return new_labels
 
-
+#%%
 start = time.time()
 images = []
 directory = 'train/train/'
@@ -50,72 +40,105 @@ plt.show()
 
 np.save('training_data', images)
 #%%
-training_data = [
-X_train = (np.concatenate([d["data"] for d in training_data], axis=0).astype("float32")) / 255
-Y_train = np.concatenate([d["labels"] for d in training_data], axis=0)
-X_train = X_train.reshape(50000, 3, 32, 32).transpose(0, 2, 3, 1)
-Y_train_input = transform_labels(Y_train)
-test_data = unpickle('cifar-10-batches-py/test_batch')
-X_test = (test_data["data"].astype("float32")) / 255
-Y_test = np.array(test_data["labels"])
-X_test = X_test.reshape(10000, 3, 32, 32).transpose(0, 2, 3, 1)
-end = time.time()
-print(f"Time for loading and preprocessing the data: {(end - start):.2f}s")
-
-
+training_data = np.load(r'C:/Users/joona/OneDrive - TUNI.fi/PRML/Assignments/DATA.ML.200_Assignment1/training_data.npy').astype("float32")
 #%%
+# Muodosta harjoitusjoukko ja sille label-joukko
+trainX = np.expand_dims(training_data / 255.0, axis=3)
+trainY = np.ones(trainX.shape[0]).astype("int32")
+for n in range(1,11):
+    trainY[(n-1)*6000 : n*6000] *= n
 
+trainY_input = transform_labels(trainY).astype("int32")
 
-#Complex model
-dropout = 0.7
+trainX, validX, trainY_input, validY_input = train_test_split(trainX, 
+                                                              trainY_input, 
+                                                              test_size=0.2,
+                                                              random_state=1)
+#%% Base model
+dropout = 0.1
+starting_filters = 16 
+kernel = (3, 3)
 model = Sequential()
 
-model.add(Conv2D(8, kernel_size=(3, 3), activation='relu', padding="same", kernel_initializer='he_uniform'))
+model.add(Conv2D(starting_filters, kernel_size=kernel, activation='relu', 
+                 kernel_initializer='he_uniform', input_shape=(28, 28, 1)))
+model.add(MaxPooling2D((2, 2)))
+model.add(Flatten())
+model.add(Dense(starting_filters*4, activation='relu', kernel_initializer='he_uniform'))
+model.add(Dense(10, activation='softmax'))
+#%% Base model
+opt = tf.optimizers.SGD(lr=0.01, momentum=0.9)
+model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
+epochs = 10
+history = model.fit(trainX, trainY_input, epochs=epochs, 
+                    validation_data=(validX, validY_input), verbose=2)
+
+#Plotting
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.grid()
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+#%% Complex model
+dropout = 0.2
+starting_filters = 8 
+kernel = (3, 3)
+model = Sequential()
+
+model.add(Conv2D(starting_filters, kernel_size=kernel, activation='relu', 
+                 padding="same", kernel_initializer='he_uniform'))
 model.add(BatchNormalization())
-model.add(Conv2D(8, kernel_size=(3, 3), activation='relu', padding="same", kernel_initializer='he_uniform'))
+model.add(Conv2D(starting_filters, kernel_size=kernel, activation='relu', padding="same", kernel_initializer='he_uniform'))
 model.add(BatchNormalization())
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(BatchNormalization())
 model.add(GaussianDropout(dropout * 0.55))
 
-model.add(Conv2D(16, kernel_size=(3, 3), activation='relu', padding="same", kernel_initializer='he_uniform'))
+model.add(Conv2D(2*starting_filters, kernel_size=kernel, activation='relu', padding="same", kernel_initializer='he_uniform'))
 model.add(BatchNormalization())
-model.add(Conv2D(16, kernel_size=(3, 3), activation='relu', padding="same", kernel_initializer='he_uniform'))
+model.add(Conv2D(2*starting_filters, kernel_size=kernel, activation='relu', padding="same", kernel_initializer='he_uniform'))
 model.add(BatchNormalization())
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(BatchNormalization())
 model.add(GaussianDropout(dropout * 0.7))
 
-model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', padding="same", kernel_initializer='he_uniform'))
+model.add(Conv2D(4*starting_filters, kernel_size=kernel, activation='relu', padding="same", kernel_initializer='he_uniform'))
 model.add(BatchNormalization())
-model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', padding="same", kernel_initializer='he_uniform'))
+model.add(Conv2D(4*starting_filters, kernel_size=kernel, activation='relu', padding="same", kernel_initializer='he_uniform'))
 model.add(BatchNormalization())
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(BatchNormalization())
 model.add(GaussianDropout(dropout * 0.85))
 
 model.add(Flatten())
-model.add(Dense(32, activation='relu', kernel_initializer='he_uniform'))
+model.add(Dense(4*starting_filters, activation='relu', kernel_initializer='he_uniform'))
 model.add(BatchNormalization())
 model.add(GaussianDropout(dropout))
 model.add(Dense(10, activation='softmax'))
 
 
-# Fitting
-epochs = 10
+#%% Fitting
+epochs = 50
 batch_size = 128
-learning_rate = 0.1
-print(f"Running {epochs} epochs with batch size of {batch_size} and learning rate of {learning_rate}.")
-tf.optimizers.Adam(lr=learning_rate)
-callback = EarlyStopping(monitor='val_accuracy', patience=10, restore_best_weights=True, mode="max")
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-history = model.fit(X_train, Y_train_input, epochs=epochs, validation_split=0.2, verbose=2,
-                    batch_size=batch_size, use_multiprocessing=True, workers=16, callbacks=[callback])
-
+learning_rate = 0.01
+print(f"Running {epochs} epochs with batch size of {batch_size} \
+      and learning rate of {learning_rate}.")
+opt = tf.optimizers.Adam(lr=learning_rate)
+callback = EarlyStopping(monitor='val_accuracy', patience=10, 
+                         restore_best_weights=True, mode="max")
+model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
+history = model.fit(trainX, trainY_input, epochs=epochs, 
+                    validation_data=(validX, validY_input), verbose=2,
+                    batch_size=batch_size, use_multiprocessing=True, 
+                    workers=16, callbacks=[callback])
+'''
 #Prediction
 y_pred = np.argmax(model.predict(X_test), axis=1)
 accuracy = class_acc(y_pred, Y_test)
-print(f"Accuracy: {accuracy}")
+print(f"Accuracy: {accuracy}")'''
 
 
 #Plotting
